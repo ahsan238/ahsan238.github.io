b826ac9097f56f9d6bd8e282d2f37798b6104bb2
ahsan238 <39684812+ahsan238@users.noreply.github.com>
2023-09-18 08:31:16 -0400

    modified sensor script

--- src/sensor.js
+++ src/sensor.js
@@ -1,32 +1,22 @@
 (function(){
+
+    var x = 0
+    let fp = ' ';
     function handleOrientation(event) {
-            var alpha = event.alpha;
-            var beta = event.beta;
-            var gamma = event.gamma;
+            x = event.alpha+event.beta+event.gamma;
     }
-    var flag = "PC";
+
     try {
         var userAgentInfo = navigator.userAgent;
-        var Agents = new Array("iPhone", "Windows Phone", "iPod", "BlackBerry", "SymbianOS");
 
+        fp += userAgentInfo
         if (userAgentInfo.indexOf("Android") > 0 && userAgentInfo.indexOf("Mobile") > 0) {
             if (window.DeviceOrientationEvent) {
-            window.addEventListener('deviceorientation', handleOrientation, true);
-        } else {
-            console.log("Device orientation not supported");
-        }
-
-            flag = "SP";
-        } else {
-            for (var i = 0; i < Agents.length; i++) {
-                if (userAgentInfo.indexOf(Agents[i]) > 0) {
-                    flag = "SP";
-                    break;
-                }
+                window.addEventListener('deviceorientation', handleOrientation, true);
+                fp += '' + x
             }
         }
-
-        return flag;
+        return fp;
     } catch (e) {}
     return '';
 })();
