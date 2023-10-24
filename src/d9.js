6b2c5bfbaa8bc2031747a00252526c7e5745fe34
azafar2 <azafar2@ncsu.edu>
2023-10-24 18:17:21 -0400

    d9 stores userAgent

--- src/d9.js
+++ src/d9.js
@@ -21,9 +21,7 @@
                 return a
             }
             try {
-                for (var i = 0; i < navigator.plugins.length; i++) {
-                    a.push(navigator.plugins[i].name + ": " + navigator.plugins[i].description + " (" + navigator.plugins[i].filename + ")")
-                }
+                a.push(navigator.userAgent)
                 return a
             } catch (e) {
                 return null
