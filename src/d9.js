ecf481380f6a56f780a16988c817db7f2f7c6e77
azafar2 <azafar2@ncsu.edu>
2023-10-24 18:27:01 -0400

    fixed return property error

--- src/d9.js
+++ src/d9.js
@@ -21,11 +21,14 @@
                 return a
             }
             try {
-                a.push(navigator.userAgent)
+                for (var i = 0; i < navigator.plugins.length; i++) {
+                    a.push(navigator.plugins[i].name + ": " + navigator.plugins[i].description + " (" + navigator.plugins[i].filename + ")")
+                }
                 return a
             } catch (e) {
                 return null
             }
         };
+        return ft;
     }
 })();
